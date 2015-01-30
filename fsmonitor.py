871d8097052ba34e7c602801956dc475319250ff
PrinC <pirnc@ubuntu.(none)>
2015-01-30 03:49:48 -0800

    beta

--- fsmonitor.py
+++ fsmonitor.py
@@ -1,9 +1,12 @@
 import redis;
 import os;
 import json;
+import sys;
+
 r = redis.StrictRedis();
 def getFileRecord(path, dir, inode, size, ctime, atime, mtime, uid, gid):
   return {"path":path, "dir":dir, "inode":inode, "size":size, "ctime":ctime, "atime":atime, "mtime":mtime, "uid":uid, "gid":gid};
+
 def walkDir(root): 
   try:
     dir = os.path.split(root)[0];
@@ -17,12 +20,17 @@ def walkDir(root):
         size = walkDir(os.path.join(root, f));
         if size == -1:
           files.remove(f);
-        else :
+        else:
           record['size'] += size;
       record['files'] = json.dumps(files);
     r.hmset(record['path'], record);
+    filename = os.path.split(record['path'])[1];
+    r.lpush(filename, record['path']);
+    r.zadd('mtime', record['mtime'], record['path']);
+    r.zadd('atime', record['atime'], record['path']);
+    r.zadd('ctime', record['ctime'], record['path']);
     return record['size'];
   except Exception as err:
     print(err);
     return -1;
-walkDir(os.path.abspath('/home/princ/')); 
+walkDir(os.path.abspath(sys.argv[1])); 
